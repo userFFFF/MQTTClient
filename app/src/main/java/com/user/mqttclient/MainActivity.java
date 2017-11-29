deb0389a4d469bcad0d9b99088fb1c924c6803d8
USR0 <user_wangjianyong@outlook.com>
2017-11-29 18:07:23 +0800

    Initial commit

---
+++ app/src/main/java/com/user/mqttclient/MainActivity.java
@@ -0,0 +1,217 @@
+package com.user.mqttclient;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.library.bubbleview.BubbleTextView;
+
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    MqttService mMqttService = new MqttService();
+    private String mBrokerUrl = "tcp://192.168.199.56:1883";
+    private String mClientID = "clintID";
+    private String mUser = "USER0";
+    private String mPassWord = "admin";
+    private String mTextPub;
+    private String mTopicName = "ANYUSER";
+    private int qos = 2;
+
+    private EditText mEditTextPub;
+    private Button mBtnPub;
+    private RecyclerView mRecyclerView;
+    private bubbleAdapter mBubbleAdapter;
+
+    List<DataModel> updateMessage = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycle_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mBubbleAdapter = new bubbleAdapter();
+        mRecyclerView.setAdapter(mBubbleAdapter);
+        initMessage();
+
+        mEditTextPub = findViewById(R.id.editText_pub);
+        mEditTextPub.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i(TAG, "beforeTextChanged");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i(TAG, "onTextChanged");
+                if (mEditTextPub.getText().length() > 0) {
+                    mBtnPub.setEnabled(true);
+                } else {
+                    mBtnPub.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i(TAG, "afterTextChanged");
+            }
+        });
+        mBtnPub = findViewById(R.id.btn_pub);
+        if (mEditTextPub.getText().length() == 0) {
+            mBtnPub.setEnabled(false);
+        } else {
+            mBtnPub.setEnabled(true);
+        }
+        mBtnPub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextPub = mEditTextPub.getText().toString();
+                Log.d(TAG, "mTextPub:" + mTextPub);
+                updateMessage(mTextPub, DataModel.TYPE_PUBLISH);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMqttService.publish(mTopicName, qos, mTextPub);
+                    }
+                }).start();
+            }
+        });
+        EventBus.getDefault().register(this);
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmsss");
+        String time = format.format(new Date());
+        mClientID = mClientID + time;
+        Log.d(TAG, "mClientID: " + mClientID);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mMqttService.init(getApplicationContext(), mBrokerUrl, mClientID, mUser, mPassWord);
+            }
+        }).start();
+    }
+
+    @Subscribe
+    public void onEvent(MqttMessage message) {
+        Log.d(TAG, "onEvent message: " + message.toString());
+        updateMessage(message.toString(), DataModel.TYPE_SUBSCRIBE);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        mMqttService.disConnect();
+        super.onDestroy();
+    }
+
+    private class bubbleAdapter extends RecyclerView.Adapter {
+        private List<DataModel> mMessageList = new ArrayList<>();
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater mInflater = LayoutInflater.from(MainActivity.this);
+            RecyclerView.ViewHolder viewHolder = null;
+            if (viewType == DataModel.TYPE_SUBSCRIBE) {
+                View mView = mInflater.inflate(R.layout.item_client, parent, false);
+                viewHolder = new clientHolder(mView);
+            } else if (viewType == DataModel.TYPE_PUBLISH) {
+                View mView = mInflater.inflate(R.layout.item_host, parent, false);
+                viewHolder = new hostHolder(mView);
+            }
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof clientHolder) {
+                ((clientHolder) holder).mBubbleTxtView.setText(this.mMessageList.get(position).mContent);
+            } else {
+                ((hostHolder) holder).mBubbleTxtView.setText(this.mMessageList.get(position).mContent);
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return this.mMessageList.get(position).mType;
+        }
+
+        @Override
+        public int getItemCount() {
+            return this.mMessageList.size();
+        }
+
+        public void setData(List<DataModel> model) {
+            this.mMessageList.addAll(model);
+        }
+
+        private class hostHolder extends RecyclerView.ViewHolder {
+            BubbleTextView mBubbleTxtView;
+
+            public hostHolder(View itemView) {
+                super(itemView);
+                mBubbleTxtView = itemView.findViewById(R.id.textView);
+            }
+        }
+
+        private class clientHolder extends RecyclerView.ViewHolder {
+            BubbleTextView mBubbleTxtView;
+
+            public clientHolder(View itemView) {
+                super(itemView);
+                mBubbleTxtView = itemView.findViewById(R.id.textView);
+            }
+        }
+    }
+
+    public void initMessage() {
+        mBubbleAdapter.setData(updateMessage);
+        mBubbleAdapter.notifyDataSetChanged();
+    }
+
+    public void updateMessage(String content, int type) {
+        DataModel dataMessage = new DataModel();
+
+        dataMessage.mContent = content;
+        dataMessage.mType = type;
+        updateMessage.add(dataMessage);
+
+        mBubbleAdapter.setData(updateMessage);
+        mBubbleAdapter.notifyDataSetChanged();
+    }
+
+    public class DataModel {
+        public static final int TYPE_PUBLISH = 1;
+        public static final int TYPE_SUBSCRIBE = 2;
+
+        public String mContent;
+        public int mType;
+    }
+}
---
+++ app/src/main/java/com/user/mqttclient/MqttService.java
@@ -0,0 +1,162 @@
+package com.user.mqttclient;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * Created by user0 on 2017/11/23.
+ */
+
+public class MqttService {
+    private static final String TAG = "MqttService";
+    private static MqttAndroidClient mClient;
+    private MqttConnectOptions mMqttConOpt;
+    //private String mClintID;
+    private String mTopicName = "ANYUSER";
+    private Context mContext;
+
+    public void init(Context context, String brokerUrl, String clientID, String userName, String password) {
+        mContext = context;
+        mClient = new MqttAndroidClient(context, brokerUrl, clientID);
+        mClient.setCallback(mMqttCallback);
+
+        mMqttConOpt = new MqttConnectOptions();
+        mMqttConOpt.setCleanSession(true);
+        mMqttConOpt.setConnectionTimeout(10);
+        mMqttConOpt.setKeepAliveInterval(20);
+        if (userName != null) {
+            mMqttConOpt.setUserName(userName);
+        }
+        if (password != null) {
+            mMqttConOpt.setPassword(password.toCharArray());
+        }
+
+        String message = "{\"terminal_uid\":\"" + clientID + "\"}";
+        mMqttConOpt.setWill(mTopicName, message.getBytes(), 0, false);
+        mMqttConOpt.setAutomaticReconnect(true);
+
+        connect();
+
+    }
+
+    public void connect() {
+        if (mClient != null && !mClient.isConnected() && isNetworkConnected()) {
+            try {
+                mClient.connect(mMqttConOpt, mContext, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.i(TAG, "connect onSuccess");
+                        subscribe(mTopicName, 2);
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.i(TAG, "connect onFailure");
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disConnect() {
+        if (mClient != null && mClient.isConnected()) {
+            try {
+                mClient.disconnect(mContext, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.i(TAG, "disconnect onSuccess");
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.i(TAG, "disconnect onFailure");
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean publish(String topicName, int qos, String payload) {
+        Log.i(TAG, "topicName: " + topicName + " qos: " + qos + " payload: " + payload);
+        return publish(topicName, qos, payload.getBytes());
+    }
+
+    public boolean publish(String topicName, int qos, byte[] payload) {
+        if (mClient != null && mClient.isConnected()) {
+            MqttMessage message = new MqttMessage(payload);
+            message.setQos(qos);
+            Log.i(TAG, "publish topicName: " + topicName + " qos: " + qos + " message: " + message.toString());
+
+            try {
+                mClient.publish(topicName, message);
+                return true;
+            } catch (MqttException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean subscribe(String topicName, int qos) {
+        if (mClient != null && mClient.isConnected()) {
+            Log.i(TAG, "subscribe topicName: " + topicName + " qos: " + qos);
+            try {
+                mClient.subscribe(topicName, qos);
+                return true;
+            } catch (MqttException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private MqttCallback mMqttCallback = new MqttCallback() {
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+            Log.i(TAG, "topic: " + topic + " message: " + message.toString());
+            EventBus.getDefault().post(message);
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken arg0) {
+            Log.i(TAG, "deliveryComplete");
+        }
+
+        @Override
+        public void connectionLost(Throwable arg0) {
+            Log.i(TAG, "connectionLost");
+        }
+    };
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        if (info != null && info.isAvailable()) {
+            String name = info.getTypeName();
+            Log.i(TAG, "Network INFO：" + name);
+            return true;
+        } else {
+            Log.i(TAG, "Network IS NOT CONNECTED");
+            return false;
+        }
+    }
+}
